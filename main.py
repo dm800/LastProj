77174ae3283988a4b2f77c39c4a20697efe4e49d
dm900 <limonov2005@bk.ru>
2022-04-30 17:59:27 +0300

    Приветствие пользователя готово

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/')
 @app.route('/index')
 def index():
-    user = " "
-    return render_template('index.html', title=' ',
-                           username=user)
+    username_from_db = 0
+    param = {}
+    param["id"] = 1
+    param["avatar"] = "avatars/id1.png"
+    param["status"] = "ученик"
+    param["username"] = username_from_db
+    param["title"] = "Школьный форум"
+    return render_template('index.html', **param)
 
 
 def main():
